5946ab8552f83637007e4b637fc32565250f6e5b
sajid <s.rahman@einhundert-energie.de>
2023-08-15 20:42:39 +0200

    removing commented code

--- demo/views.py
+++ demo/views.py
@@ -1,12 +1,8 @@
-import redis
 from django.http import HttpResponse
-from django.shortcuts import render
-
 from demo.models import Invoice
-from demo.tasks import singleton_task, another_task
+from demo.tasks import singleton_task
 
 
-# Create your views here.
 def hello(request, *args, **kwargs):
     current_count = Invoice.objects.count()
     for i in range(10):
